444e973af980fd6171c8a5e144baac8149f47172
Aleksey Svechkar <asvechkar@freshintel.ru>
2025-03-04 15:13:55 +0300

    Add knight player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,20 +10,36 @@ namespace Player
         private static readonly int Speed = Animator.StringToHash("Speed");
         private static readonly int LastDirectionX = Animator.StringToHash("LastDirectionX");
         private static readonly int LastDirectionY = Animator.StringToHash("LastDirectionY");
+        private static readonly int IsRunning = Animator.StringToHash("IsRunning");
 
         [SerializeField] private float speed;
+        [SerializeField] private float runSpeed;
         [SerializeField] private Animator animator;
         
         private Vector3 _movement;
+        private bool _isRunning;
         public Vector3 Movement => _movement;
         
         private Vector2 _lastDirection;
 
         private void Update()
         {
-            transform.position += _movement.normalized * (speed * Time.deltaTime);
+            var currentSpeed = speed;
+            
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed = runSpeed;
+                _isRunning = true;
+            }
+            else
+            {
+                _isRunning = false;
+            }
+            
+            transform.position += _movement.normalized * (currentSpeed * Time.deltaTime);
             
             animator.SetFloat(Horizontal, _movement.x);
+            animator.SetBool(IsRunning, _isRunning);
             animator.SetFloat(Vertical, _movement.y);
             animator.SetFloat(Speed, _movement.sqrMagnitude);
             animator.SetFloat(LastDirectionX, _lastDirection.x);
